2d50a0b8d217fee41a4646301316a727eb90c681
Majeed <majeedk526@gmail.com>
2016-09-10 16:04:45 -0800

    added comments in the code

--- cost.m
+++ cost.m
@@ -1,5 +1,6 @@
 function c = cost(m, h, y)
-
+    %calculate cost for the value of theat used to calculate hypothesis
+    %points
     c = 1/(2*m) * sum((h-y).^2);
 
 end
--- hypothesis.m
+++ hypothesis.m
@@ -1,5 +1,6 @@
 function h = hypothesis(X, theta)
-
+    %multiply theta to the array X to calculate points to plot hypothesis
+    %function
     h = theta * X;
 
 end
